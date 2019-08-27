bf768171f6497631e78aa166db631d4c0636e871
FERNANDO <fer97_23@hotmail.com>
2019-08-27 13:33:03 -0500

    Pizza Terminada

--- 06.pizza/Program.cs
+++ 06.pizza/Program.cs
@@ -12,15 +12,44 @@ namespace _06.pizza
 
             if (args.Length<4)
             {
-                Conosole.Clear();
+                Console.Clear();
                 Console.WriteLine("Debes especificar como quieres tu pizza \n");
                 Console.WriteLine("<Tamano> <Ingredientes> <Cubierta> <Para>");
                 Menu();
                 return 1;
-
             }
 
+            //tamano
+            tam=char.Parse(args[0]);
+            if (tam=='P') tamano="Pequeño";
+            else if(tam=='M') tamano="Mediana";
+            else tamano = "Grande";
 
+            //ingredientes
+            ing= args[1].Split("+");
+            foreach(string i in ing){
+                if(i=="E") ingredientes+="Extra Queso ";
+                else if(i=="C") ingredientes+="Champinones ";
+                else if(i=="P") ingredientes+="Piña ";
+            }
+            
+            //cubierta
+            cub=char.Parse(args[2]);
+            if (cub=='D') cubierta="Delgada";
+            else cubierta = "Gruesa";
+
+            //Para
+            par=char.Parse(args[3]);
+            if (par=='C') para="Comer Aqui";
+            else para = "Para llevar";
+
+
+            //imprima la orden
+            Console.WriteLine("Tu pizza quedo de la siguiente forma:  \n");
+            Console.WriteLine("Tamano "+tamano+"\n");
+            Console.WriteLine("Ingredientes "+ingredientes+"\n");
+            Console.WriteLine("Cubierta "+cubierta+"\n");
+            Console.WriteLine("Para "+para+"\n");
             return 0;
         }
 
