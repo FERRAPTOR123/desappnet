3756ff1a56881b2cda1b70e0a80416b42c61f8b4
FERNANDO <fer97_23@hotmail.com>
2019-08-29 12:34:31 -0500

    Programa 7

---
+++ 08.vectorPromedio/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _08.vectorPromedio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vector = 
+            {10,20,30,40,50,60,70,80,90,100,
+            10,20,30,40,50,60,70,80,90,100,
+            10,20,30,40,50,60,70,80,90,100,
+            10,20,30,40,50,60,70,80,90,100,};
+
+            int suma=0, nmp=0;
+            float promedio=0;
+            
+            for(int i=0; i<vector.Length;i++){
+                Console.WriteLine($"{vector[i]} ");
+                suma+= vector[i];
+            }
+
+            promedio=suma/vector.Length;
+            Console.WriteLine($"El promedio es{nmp}\n");
+
+            Console.WriteLine("Numeros mayor que el promedio\n");
+            for(int i=0; i<vector.Length;i++){
+                if (vector[i]>promedio)
+                {
+                    Console.WriteLine($"{vector[i]} ");
+                    nmp++;
+                }
+            }
+
+            Console.WriteLine($"Mayores que el promedio {nmp}");
+        }
+    }
+}
